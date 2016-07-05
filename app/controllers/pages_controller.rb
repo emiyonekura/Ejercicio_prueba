f7f47beb414ae985595008dad560347cf6abd74f
Emiko <emiko.yonekura@gmail.com>
2016-07-04 20:24:29 -0400

    form ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def x
+  	@nombre = params[:nombre]
+  	@email = params[:email]
+  	@edad = params[:edad]
+
+  	@usuario = Usuario.create(nombre: @nombre, email: @email, edad: @edad)
   end
 end
